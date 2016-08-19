4880a5ec0be7f372a7a0d920bf78414dd0c4036f
Daniel Montgomery <damontgomery@gmail.com>
2016-08-19 15:57:26 -0500

    Route and Argument examples

---
+++ d8e_route_argument/src/Controller/DemoController.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\d8e_route_argument\Controller\DemoController.
+ */
+
+namespace Drupal\d8e_route_argument\Controller;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\user\UserInterface;
+
+/**
+ * Controller routines for page example routes.
+ */
+class DemoController extends ControllerBase {
+  /**
+   * Constructs a simple page.
+   *
+   * The router _controller callback, maps the path 'd8e/route-argument-text-demo/{myArgument}'
+   * to this method.
+   *
+   * This argument is passed in, as text to the
+   *
+   * @param string $myArgument
+   *
+   * @return array
+   */
+  public function textDemo($myArgument) {
+    return [
+      '#markup' => '<p>' . t('d8e Route & Argument Text Demo: @myArgument', ['@myArgument' => $myArgument]) . '</p>',
+    ];
+  }
+
+  /**
+   * Constructs a simple page.
+   *
+   * The router _controller callback, maps the path 'd8e/route-argument-user-demo/{user}'
+   * to this method.
+   *
+   * This argument is passed in and converted to a user object since Drupal recognizes the pattern {user} in the route.
+   *
+   * @param \Drupal\user\UserInterface $user
+   *
+   * @return array
+   */
+  public function userDemo(UserInterface $user) {
+    return [
+      '#markup' => '<p>' . t('d8e Route & Argument User Demo: @user', ['@user' => $user->getDisplayName()]) . '</p>',
+    ];
+  }
+}
